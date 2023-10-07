1b523c1e20e11178f17f839f9a636f4b6975a599
Elgun <eabdulaev12@mail.ru>
2023-10-08 02:26:58 +0300

    commit

---
+++ src/Homework/HanoiTower.java
@@ -0,0 +1,19 @@
+package Homework;
+
+public class HanoiTower {
+    public static void main(String[] args) {
+        int nDisks = 3;
+        doTowers(nDisks, 'A', 'B', 'C');
+    }
+    public static void doTowers(int disk, char A, char B, char C) {
+        if (disk == 1) {
+            System.out.println("Диск 1 от " + A + " до " + C);
+        } else {
+            doTowers(disk - 1, A, C, B);
+            System.out.println("Диск " + disk + " от " + A + " до " + C);
+            doTowers(disk - 1, B, A, C);
+        }
+    }
+}
+
+// итеративно не смог
